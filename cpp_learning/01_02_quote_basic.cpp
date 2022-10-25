60bc910a72f0564a55d46ef1823d25134d94374f
qwang-77 <1061673783@qq.com>
2022-10-24 20:42:21 -0700

    Add project files.

---
+++ cpp_learning/01_02_quote_basic.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+// main() is a necessary function in C++
+int main()
+{
+    // print hello c++ on the screen
+    cout << "hello C++" << endl;
+
+    system("pause");
+    
+    return(0);
+}
---
+++ cpp_learning/04_constant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// #define name value  -- This is to define a macro constant
+	// const type name = value -- This is to define a usual way to define constant
+
+	system("pause");
+	return 0;
+
+}
