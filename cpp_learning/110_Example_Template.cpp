be408ae69dd201eecd885c41e9bf9bce887c5baa
qwang-77 <1061673783@qq.com>
2023-01-22 13:22:22 -0800

    update notes

--- cpp_learning/110_Example_Template.cpp
+++ cpp_learning/110_Example_Template.cpp
@@ -35,6 +35,17 @@ void mysort(T& arr, int len)
 	}
 }
 
+// Print template
+template<class T>
+void printarray(T arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
 
 void test01()
 {
@@ -42,11 +53,22 @@ void test01()
 	char chararr[] = "badcfe";
 	int num = sizeof(chararr) / sizeof(char);
 	mysort(chararr,num);
+	printarray(chararr, num);
+	
+}
+
+void test02()
+{
+	int intarr[] = { 7,5,1,3,9,2,4,6,8 };
+	int num = sizeof(intarr) / sizeof(int);
+	mysort(intarr, num);
+	printarray(intarr, num);
 }
 
 int main() {
 
-
+	test01();
+	test02();
 	system("pause");
 	return 0;
 }
---
+++ cpp_learning/111_Difference_Between_Template_and_Function.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// Difference between common function and template
+
+// 1. Common function can transfer the implicit type
+// 2. Template can derive itself, cannot transfer the implicit type
+// 3. Template showing specific type, can transfer the implicit type.
+
+// Common function
+
+int myadd(int a, int b)
+{
+	return a + b;
+}
+
+void test01()
+{
+	int a = 10;
+	int b = 20;
+	char c = 'a'; // ASC code: a-97, c-99, so they transfered c to 99 automatically
+	cout << myadd(a, b) << endl;
+	cout << myadd(a, c) << endl;
+}
+
+template<class T>
+T myadd02(T a, T b)
+{
+	return a + b;
+}
+
+void test02()
+{
+	int a = 10;
+	int b = 20;
+	char c = 'a';
+	cout << myadd02(a, b) << endl;
+	// cout << myadd02(a, c) << endl; Wrong, cuz it cannot transfer implicitly
+	cout << myadd02<int>(a, c) << endl;
+}
+
+int main() {
+	test01();
+	system("pause");
+	return 0;
+}
